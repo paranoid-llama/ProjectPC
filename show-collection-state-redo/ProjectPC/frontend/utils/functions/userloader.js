7b17d518fbc62e2a9cba986f3c6d60cc9290afb6
paranoid-llama <omar_born@hotmail.com>
2024-05-29 10:21:30 -0400

    re-ordered cuz im an idiot again

--- show-collection-state-redo/ProjectPC/frontend/utils/functions/userloader.js
+++
@@ -1,6 +0,0 @@
-export default async function userLoader({params}) {
-    const user = fetch(`http://localhost:3000/users/${params.id}`)
-                            .then((res) => res.json())
-                            .then((data) => data)
-    return user
-}
