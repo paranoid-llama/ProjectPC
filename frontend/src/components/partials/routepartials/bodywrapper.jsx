ad29819b1ad116ee9d77df946b15e5506df9c5bb
paranoid-llama <omar_born@hotmail.com>
2024-08-25 18:25:05 -0400

    frontend only editing

--- frontend/src/components/partials/routepartials/bodywrapper.jsx
+++
@@ -1,9 +0,0 @@
-import {Box} from '@mui/material'
-
-export default function BodyWrapper({children, sx}) {
-    return (
-        <Box sx={{flexGrow: 1, margin: 5, textAlign: 'center', ...sx}}>
-            {children}
-        </Box>
-    )
-}
--- frontend/utils/functions/eggmoves/getmovestyles.js
+++
@@ -1,8 +0,0 @@
-import {moveTypes, typeStyles} from './movetypecolors.js'
-
-const getMoveStyles = (moveName) => {
-    const moveStyles = typeStyles[moveTypes[moveName]]
-    return moveStyles
-}
-
-export default getMoveStyles
