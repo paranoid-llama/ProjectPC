2fb8d8e3fc4bf7db34e6bd0cff202d39c6d03a55
paranoid-llama <omar_born@hotmail.com>
2024-10-14 07:02:13 -0400

    post-update 6 oct-14-2024
    random effort.

--- frontend/utils/functions/backendrequests/users/getsession.js
+++ frontend/utils/functions/backendrequests/users/getsession.js
@@ -6,28 +6,21 @@ const intServerError = {
 const backendurl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000'
 
 //<!doctype is not valid json
-console.log(backendurl)
 
 export default async function getSession() {
-    try {
-        const userData = await fetch(`${backendurl}/api/session`, {
-            method: 'GET',
-            credentials: 'include',
-            headers: {
-                'Access-Control-Allow-Origin': 'https://pokellections.koyeb.app'
-            },
-        }).then(async(res) => {
-            if (res.status === 500) {throw intServerError}
-            const data = await res.json()
-            if (res.ok) {return data} 
-            else {throw data}
-        }) 
-        const userIsLoggedIn = Object.keys(userData).length !== 0
-        const loggedInData = userIsLoggedIn ? {loggedIn: true, user: userData} : {loggedIn: false}
-        return loggedInData 
-    }
-    catch(e) {
-        console.log(e)
-        return {loggedIn: false}
-    }
+    const userData = await fetch(`${backendurl}/api/session`, {
+        method: 'GET',
+        credentials: 'include',
+        // headers: {
+        //     'Access-Control-Allow-Origin': 'https://pokellections.koyeb.app'
+        // },
+    }).then(async(res) => {
+        if (res.status === 500) {throw intServerError}
+        const data = await res.json()
+        if (res.ok) {return data} 
+        else {throw data}
+    }) 
+    const userIsLoggedIn = Object.keys(userData).length !== 0
+    const loggedInData = userIsLoggedIn ? {loggedIn: true, user: userData} : {loggedIn: false}
+    return loggedInData 
 }
