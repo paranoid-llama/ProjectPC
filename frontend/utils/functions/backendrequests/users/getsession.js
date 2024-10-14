99d48f5874effd4e89d26fa97925b172dc8d29b7
paranoid-llama <omar_born@hotmail.com>
2024-10-14 06:32:14 -0400

    post-update 4 oct-14 2024
    doing a try catch i guess... to see what the hell is going on in prod...

--- frontend/utils/functions/backendrequests/users/getsession.js
+++ frontend/utils/functions/backendrequests/users/getsession.js
@@ -6,19 +6,25 @@ const intServerError = {
 const backendurl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000'
 
 export default async function getSession() {
-    const userData = await fetch(`${backendurl}/api/session`, {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-            'Access-Control-Allow-Origin': 'https://pokellections.koyeb.app'
-        },
-    }).then(async(res) => {
-        if (res.status === 500) {throw intServerError}
-        const data = await res.json()
-        if (res.ok) {return data} 
-        else {throw data}
-    }) 
-    const userIsLoggedIn = Object.keys(userData).length !== 0
-    const loggedInData = userIsLoggedIn ? {loggedIn: true, user: userData} : {loggedIn: false}
-    return loggedInData 
+    try {
+        const userData = await fetch(`${backendurl}/api/session`, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Access-Control-Allow-Origin': 'https://pokellections.koyeb.app'
+            },
+        }).then(async(res) => {
+            if (res.status === 500) {throw intServerError}
+            const data = await res.json()
+            if (res.ok) {return data} 
+            else {throw data}
+        }) 
+        const userIsLoggedIn = Object.keys(userData).length !== 0
+        const loggedInData = userIsLoggedIn ? {loggedIn: true, user: userData} : {loggedIn: false}
+        return loggedInData 
+    }
+    catch(e) {
+        console.log(e)
+        return {loggedIn: false}
+    }
 }
